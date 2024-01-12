d4dcabc77f5c57e4d82cf885b9ca10eb2400bca5
chertovich-dev <chertovich.dev@gmail.com>
2024-01-12 14:12:49 +0300

    deleted TestActivity

--- app/src/main/java/com/dikiyserge/glassmosaic/view/MainActivity.kt
+++ app/src/main/java/com/dikiyserge/glassmosaic/view/MainActivity.kt
@@ -1,41 +1,17 @@
 package com.dikiyserge.glassmosaic.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.dikiyserge.glassmosaic.viewmodel.MainViewModel
 import com.dikiyserge.glassmosaic.R
-import com.dikiyserge.glassmosaic.TestActivity
-import com.dikiyserge.glassmosaic.data.Element
-import com.dikiyserge.glassmosaic.data.Mosaic
-import com.dikiyserge.glassmosaic.data.Triangle
 import com.dikiyserge.glassmosaic.databinding.ActivityMainBinding
-import com.dikiyserge.glassmosaic.getParcelableData
 import com.dikiyserge.glassmosaic.model.MainRepository
 import com.dikiyserge.glassmosaic.viewmodel.MainToMosaicNav
 import com.dikiyserge.glassmosaic.viewmodel.MainViewModelFactory
 import com.dikiyserge.glassmosaic.viewmodel.Nav
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.async
 
 private const val TAG = "_mosaic"
 
@@ -44,7 +20,6 @@ const val KEY_MOSAIC = "mosaic"
 fun log(text: String, tag: String = TAG) = Log.i(tag, text)
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels { MainViewModelFactory(MainRepository(applicationContext)) }
 
@@ -67,8 +42,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
 
         viewModel.navLiveEvent.observe(this) { nav ->
             navigate(nav)
